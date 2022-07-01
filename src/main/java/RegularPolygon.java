524b904e7cd166543fe9455ba2f0d46894c3eb6c
Marc Murray <mmarc.96@gmail.com>
2022-07-01 12:01:44 +0100

    Initial commit

---
+++ src/main/java/RegularPolygon.java
@@ -0,0 +1,12 @@
+public abstract class RegularPolygon {
+	private String description;
+	private int sideLength;
+	private int numberOfSides;
+	int getArea(){
+		return 0;
+	}
+	String displayDetails(){
+		return "";
+	}
+
+}
